d50024984012ad94e06b5ed75828cc5c4098c067
mgh3326 <mgh3326@naver.com>
2017-12-27 10:55:33 +0900

    complelte

--- nextSqaure.js
+++ nextSqaure.js
@@ -1,8 +1,9 @@
 function nextSqaure(n){
-    var result = 0;
-    //함수를 완성하세요
 
-    return result;
+    //함수를 완성하세요
+    if((Math.sqrt(n))===parseInt((Math.sqrt(n))))
+    return Math.pow((Math.sqrt(n)+1),2);
+    return "no";
 }
 
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
